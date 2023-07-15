9c82d13a1c694cbd0018585b5de31fe0632fd172
thraneh <thraneh@gmail.com>
2023-07-15 08:12:05 +0200

    flags clean-up

--- include/roq/flags/validators.hpp
+++ include/roq/flags/validators.hpp
@@ -4,10 +4,5 @@
 
 #include "roq/flags/validators/directory.hpp"
 #include "roq/flags/validators/listen_address.hpp"
-#include "roq/flags/validators/non_empty.hpp"
-#include "roq/flags/validators/non_zero.hpp"
 #include "roq/flags/validators/path.hpp"
-#include "roq/flags/validators/positive.hpp"
-#include "roq/flags/validators/required_listen_address.hpp"
-#include "roq/flags/validators/required_path.hpp"
 #include "roq/flags/validators/time_period.hpp"
